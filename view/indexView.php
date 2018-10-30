0bd8bc45fd3ce51b026081180cbfbe6b393068b3
Bopha TIN <Bo@Bophas-iMac.local>
2018-10-30 13:47:14 +0100

    Ajout home page

---
+++ view/indexView.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<html lang="fr-FR">
+
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="public/style.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+	<title>Jean Forteroche</title>
+</head>
+
+
+
+<body>
+
+	<header>
+		<h1 class="title">Jean Forteroche</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php?p=allPostsView">Articles</a></li>
+				<li>Biographie</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	</header>
+
+	<section id="wrapper-1">
+		<div class="container-1">
+			<img src="https://1.bp.blogspot.com/-XkBlFe4aJfs/WyvCukbVOKI/AAAAAAAAoOQ/6-abJfWj7cgBTlY1b0NKorI7pzFEuo1VACLcBGAs/s1600/recessed_bathtub_clay_tiles_casa_modesta_par_photo_joao_carmo_simoes_yatzer.jpg" class="left-bloc"/>
+			<div class="right-bloc">
+				<p>Jean Forteroche est né à Genève en 1985.<br/><br/>
+				Son deuxième roman, La Vérité sur l’Affaire Harry Quebert, paraît en septembre 2012. Ce roman, traduit en 40 langues et vendu à 5 millions d’exemplaires à travers le monde, sera récompensé en France du Prix de la Vocation de la Fondation Bleustein-Blanchet, du Grand Prix du roman de l’Académie française et du Prix Goncourt des Lycéens.</p>
+			</div>
+		</div>
+	</section>
+
+	<section id="wrapper-2">
+
+		<div class="container-2">
+			<div class="left-art">
+				<h2>Latest Posts</h2>
+				<p class="txt-post">Jusqu’au jour du Drame, il y avait deux familles Goldman. Les Goldman-de-Baltimore et les Goldman-de-Montclair. Les Goldman-de-Montclair, dont est issu Marcus Goldman, l’auteur de La Vérité sur l’Affaire Harry Quebert, sont une famille de la classe moyenne, habitant une petite maison à Montclair, dans le New Jersey. Les Goldman-de-Baltimore sont une famille prospère à qui tout sourit, vivant dans une luxueuse maison d’une banlieue riche de Baltimore, à qui Marcus vouait une admiration sans borne. <br/><br/>
+				Huit ans après le Drame, c’est l’histoire de sa famille que Marcus Goldman décide cette fois de raconter, lorsqu’en février 2012, il quitte l’hiver new-yorkais pour la chaleur tropicale de Boca Raton, en Floride, où il vient s’atteler à son prochain roman. <br/><br/>
+				Au gré des souvenirs de sa jeunesse, Marcus revient sur la vie et le destin des Goldman-de-Baltimore et la fascination qu’il éprouva jadis pour cette famille de l’Amérique huppée, entre les vacances à Miami, la maison de vacances dans les Hamptons et les frasques dans les écoles privées. Mais les années passent et le vernis des Baltimore s’effrite à mesure que le Drame se profile. Jusqu’au jour où tout bascule. <br/> <br/>
+				Et cette question qui hante Marcus depuis : qu’est-il vraiment arrivé aux Goldman-de-Baltimore ?</p>
+				<button><a href="index.php?p=allPostsView">Voir tous les articles</a></button>				
+			</div>
+			<div class="right-art">
+				<div class="container-art">
+					<div class="art-1">
+						<div class="num-art"><span>01</span></div>
+						<article class="art"></article>
+					</div>
+					<div class="art-1">
+						<div class="num-art"><span>02</span></div>
+						<article class="art"></article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="wrapper-3">
+		<h3 class="title-3">Meet me</h3>
+		<div class="container-3">
+			<div class="left-tab">
+				<p>Autograph Tour</p>
+			</div>
+			<div class="right-tab">
+				<table>
+					<tr>
+						<td>Paris</td>
+						<td>Hôtel de Ville</td>
+						<td>22 février 2019</td>
+					</tr>
+					<tr>
+						<td>Bhoutan</td>
+						<td>Taktshang</td>
+						<td>30 mars 2019</td>
+					</tr>
+					<tr>
+						<td>Tel Aviv</td>
+						<td>Jaffa</td>
+						<td>12 mai 2019</td>
+					</tr>
+					<tr>
+						<td>Phnom Penh</td>
+						<td>Palais Royal</td>
+						<td>9 juillet 2019</td>
+					</tr>
+					<tr>
+						<td>Bogota</td>
+						<td>Place Bolivar</td>
+						<td>10 octobre 2019</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</section>
+
+</body>
+
+
+
+<footer></footer>
+
+
+</html>
