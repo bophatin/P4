3b036f23d8e4ce6e327550de3f6a282d85a95080
Bopha TIN <Bo@Bophas-iMac.local>
2018-11-14 16:29:57 +0100

    Ajout vue de la page connexion au BO

---
+++ view/connexView.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="connex.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+	<title>Back Office - Forteroche</title>
+</head>
+
+
+
+<body>
+	<header>
+		<h1>Back office</h1>
+	</header>
+
+	<section id="form">
+		<div class="background-form">
+			<form method="post" action="">
+			    <label for="pseudo">PSEUDO</label>
+			    <input type="text" name="pseudo" id="nom"/>
+			    <label for="password">MOT DE PASSE</label>
+			    <input type="password" name="mdp" id="mdp"/>
+			    <input type="submit" name="envoyer" value="VALIDER" class ="button"/>
+			</form> 
+		</div>
+	</section>
+</body>
+
+
+
+<footer>
+	<p class="footer-name">Jean Forteroche, 2018.</p>
+</footer>
+
+
+</html>
