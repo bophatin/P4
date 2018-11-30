e7fe6467a034e5b67f0feed94ee879144b8c5d44
Bopha TIN <Bo@Bophas-iMac.local>
2018-11-30 14:25:17 +0100

    Appels controller pages users + post

--- admin.php
+++ admin.php
@@ -13,6 +13,7 @@ if (isset($_GET['p'])) {
 }
 
 
+// PAGE LOG
 
 if ($p === 'logView') {
 	require 'view/back/logView.php';
@@ -28,20 +29,76 @@ if ($p === 'logView') {
 }
 
 
+// PAGE USERS 
+
 if ($p === 'usersView') {
 
-	if (isset($_POST['pseudo-add'], $_POST['mdp-add'])) {
-		if (!empty($_POST['pseudo-add']) AND !empty($_POST['mdp-add'])) {
-			AdminController::addUser();
-		} else {
-			echo "Veuillez remplir tous les champs";
+	if(isset($_POST['create'])) {
+		if (isset($_POST['pseudo-add'], $_POST['mdp-add'])) {
+			if (!empty($_POST['pseudo-add']) AND !empty($_POST['mdp-add'])) {
+				AdminController::addUser();
+				header('Location: admin.php?p=usersView'); 
+			} else {
+				echo "Veuillez remplir tous les champs";
+			}
 		}
-	}
+	} 
 
 	AdminController::getListUsers();
 
+
 	if (isset($_GET['id'])) {
-		AdminController::deleteUser();
+		AdminController::deleteUser(); 
 	}
 
+	if(isset($_POST['update'])) {
+		if (isset($_GET['name_admin'])) {
+			AdminController::getUserId();
+		} else {
+			die('Erreur'); 
+		}
+	}
+
+	if(isset($_POST['save'])) {
+		if (isset($_POST['pseudo'], $_POST['mdp'])) {
+			AdminController::updateUser();
+			header('Location: admin.php?p=usersView'); 
+		} 
+	}
 }
+
+
+// PAGE POST
+
+if ($p === 'postEditView') {
+
+	if(isset($_POST['addPost'])) {
+		if (isset($_POST['title'], $_POST['content'])) {
+			if (!empty($_POST['title']) AND !empty($_POST['content'])) {
+				AdminController::addPost();
+				header('Location: admin.php?p=postEditView'); 
+			} else {
+				echo "Veuillez remplir tous les champs";
+			}
+		}
+	} 
+
+	AdminController::getLists();
+
+	if (isset($_GET['id'])) {
+		AdminController::deleteComment(); 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
