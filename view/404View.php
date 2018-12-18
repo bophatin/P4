f0a2e4e0fde2ac923d579472ff861690e8e10ae8
Bopha TIN <Bo@Bophas-iMac.local>
2018-12-18 15:34:13 +0100

    Création page 404

---
+++ view/404View.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="fr-FR">
+
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="public/404.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+	<title>PAGE ERREUR - Jean Forteroche</title>
+</head>
+
+
+
+<body>
+
+	<header>
+		<h1 class="title">Jean Forteroche</h1>
+	</header>
+
+	<div class="container-1">
+		<div class="carre">On a perdu sa route ?<br/>Come on, let's go home <a href="index.php"><i class="fas fa-long-arrow-alt-right"></i></a></div>
+		<div class="carre-big">404</div>
+	</div>
+
+
+
+</body>
+
+
+
+<footer></footer>
+
+
+</html>
