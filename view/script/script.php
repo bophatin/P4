f25a0d617ed842a85d59a9cf7e7d4b19c658996f
Bopha TIN <Bo@Bophas-iMac.local>
2018-12-21 13:27:52 +0100

    Ajout fichier passerelle pour gérer bouton signaler commentaire

---
+++ view/script/script.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../../model/Database.php';
+require '../../model/Comment.php';
+require '../../model/CommentManager.php';
+
+
+if(isset($_GET['id']) AND !empty($_GET['id'])) {
+
+	$id = htmlspecialchars($_GET['id']);
+	$signaler = '';
+	$signaler++;
+	
+
+	$newSignComm = new Comment([
+		'signaler' => $signaler,
+		'id' => htmlspecialchars($_GET['id'])
+	]);
+
+	$NSM = new CommentManager();
+	$upSignaler = $NSM->upSignaler($newSignComm);
+
+	$alert = "<script> alert('Votre commentaire a bien été signalé') </script>";
+	echo "$alert";
+
+
+	/*header('Location: ' .$_SERVER['HTTP_REFERER']);*/
+
+
+
+}
+
+
+
+	
+		
